8c734c25b64717074031928ca30ca7b6e4d7b594
Edward Yuan <yuan.bo.august@gmail.com>
2023-03-03 15:08:26 +0800

    modify types of CacheServer data

--- src/CacheServer/cacheServer.go
+++ src/CacheServer/cacheServer.go
@@ -3,6 +3,7 @@ package cacheserver
 // 缓存服务器用来快速存取对于性能要求较高的数据，例如战斗时需要获取玩家数据
 // 好友关系或公会数据等
 type CacheServer struct {
-	BaseData     []byte
-	RelationData []byte
+	BaseData     interface{} // 后续改成对应的数据结构类型
+	RelationData interface{}
+	/* …… */
 }
